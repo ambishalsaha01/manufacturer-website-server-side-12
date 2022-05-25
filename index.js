e1a8206e89cf181347f29468256c1118eefbae96
ambishalsaha01 <bishalsaha9989@gmail.com>
2022-05-25 12:22:53 -0700

    load single part data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,29 +13,37 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const partCollection = client.db('car_manufacturer').collection('parts');
         const reviewCollection = client.db('car_manufacturer').collection('reviews');
 
         // get part data from mongodb
-        app.get('/part', async (req, res)=>{
-            const query = { };
+        app.get('/part', async (req, res) => {
+            const query = {};
             const cursor = partCollection.find(query);
             const parts = await cursor.toArray();
             res.send(parts);
         })
 
+        // Get single part data
+        app.get('/part/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await partCollection.findOne(query);
+            res.send(service);
+        })
+
         // get part data from mongodb
-        app.get('/review', async (req, res)=>{
-            const query = { };
+        app.get('/review', async (req, res) => {
+            const query = {};
             const cursor = reviewCollection.find(query);
             const reviews = await cursor.toArray();
             res.send(reviews);
         })
     }
-    finally{
+    finally {
 
     }
 }
